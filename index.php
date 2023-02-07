d3543418a73eb48101a3553e8c413b5a5f132616
Jack Zeng <zeng.zhenhuai@student.greenriver.edu>
2023-02-07 17:18:50 -0500

    Build info page

--- index.php
+++ index.php
@@ -14,9 +14,32 @@ require_once('vendor/autoload.php');
 //Instantiate F3 Base class
 $f3 = Base::instance();
 
-$f3->route('GET /', function(){
+$f3->route('GET /', function($f3){
 
     //Instantiate a view
+    $f3->set('username', 'jshmo');
+    $f3->set('password', sha1('Password01'));
+    $f3->set('title', 'Working with Templates');
+
+    //Add your fav food and color
+    $f3->set('favfood','hamburger');
+    $f3->set('color','red');
+
+    //radius
+    $f3->set('radius', 10);
+
+    //Define an array of fruits
+    $fruits = array("apple","banana","orange");
+
+//    foreach($fruits as $fruit)
+//    {
+//        echo $fruit;
+//    }
+
+    $f3->set('fruits',$fruits);
+
+    $f3->set('colors',array("red","blue","yellow"));
+
     $view = new Template();
     echo $view -> render("views/info.html");
 });
